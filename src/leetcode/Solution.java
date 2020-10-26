f598ed147d59ab58bf12cc679a8f203c98771d65
a495800763 <495800763@qq.com>
2020-10-27 00:05:14 +0800

    leetcode 和为s 的连续正数序列

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -2,24 +2,27 @@ package leetcode;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] a = new int[5];
-        a[0] = 8;
-        a[1] = 1;
-        a[2] = 2;
-        a[3] = 2;
-        a[4] = 3;
+//        int[] a = new int[5];
+//        a[0] = 8;
+//        a[1] = 1;
+//        a[2] = 2;
+//        a[3] = 2;
+//        a[4] = 3;
+//
+//        int[] ints = smallerNumberThanCurrent(a);
+//
+//        for (int anInt : ints) {
+//            System.out.print(anInt);
+//            System.out.print(",");
+//        }
+//
+//        System.out.println('\n');
 
-        int[] ints = smallerNumberThanCurrent(a);
-
-        for (int anInt : ints) {
-            System.out.print(anInt);
-            System.out.print(",");
-        }
-
-        System.out.println('\n');
+        System.out.println(findContinuousSequence(15));
 
     }
 
@@ -49,6 +52,41 @@ public class Solution {
             }
             result[i] = currentResult;
 
+        }
+        return result;
+    }
+
+    /**
+     * leetcode 和位s 的连续正数序列
+     *
+     * @param target
+     * @return
+     */
+    public static int[][] findContinuousSequence(int target) {
+        Map<Integer, int[]> map = new TreeMap<>();
+        for (int i = 2; i < target; i++) {
+            // 项数 i
+            if (target * 2 % i == 0) {
+                for (int j = target / 2; j >= 1; j--) {
+                    //首项 j
+                    // 末项 j+i-1
+                    if ((j + j + i - 1) * i / 2 == target) {
+                        int[] current = new int[i];
+                        for (int k = 0; k < i; k++) {
+                            current[k] = j + k;
+                        }
+                        map.put(j, current);
+                        break;
+                    }
+                }
+            }
+        }
+
+        int i = 0;
+        int[][] result = new int[map.size()][];
+        for (Map.Entry<Integer, int[]> entry : map.entrySet()) {
+            result[i] = entry.getValue();
+            i++;
         }
         return result;
     }
